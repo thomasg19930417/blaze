80b8f57949e0876093fde4cb73d38507c73d5773
xu.guo <570736711@qq.com>
2025-08-20 16:25:16 +0800

    Update release-docker.sh

--- release-docker.sh
+++ release-docker.sh
@@ -4,5 +4,6 @@ export SHIM="${SHIM:-spark-3.0}"
 export MODE="${MODE:-release}"
 export JAVA_VERSION="${JAVA_VERSION:-8}"
 export SCALA_VERSION="${SCALA_VERSION:-2.12}"
+export CELEBORN_VERSION="${CELEBORN_VERSION:-0.5}"
 
 docker-compose -f dev/docker-build/docker-compose.yml up
